487151106a125734ebfde4cea0f616546203bf63
GoatBoy <cordell.phil.garcia@gmail.com>
2025-04-02 01:11:10 +0800

    Major changes to the gmail sending functionality
    I found a simpler and more efficient way of sending mail

--- despatt_proj_grp2/src/main/java/com/keebcove/utility/GmailService.java
+++ despatt_proj_grp2/src/main/java/com/keebcove/utility/GmailService.java
@@ -1,84 +1,44 @@
 package com.keebcove.utility;
 
-import com.google.api.client.http.ByteArrayContent;
-import com.google.api.client.util.Base64;
-import com.google.api.services.gmail.Gmail;
-import com.google.api.services.gmail.model.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.Properties;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
-import java.security.GeneralSecurityException;
-import java.util.Set;
-import static com.google.api.services.gmail.GmailScopes.GMAIL_SEND;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 
 public class GmailService {
-
-    private static Credential getCredentials(final NetHttpTransport httpTransport, GsonFactory jsonFactory)
-            throws IOException {
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(GmailService.class.getResourceAsStream("client_secret_460183509987-9fhgcprc4kp6c8pvbf9770itaa3lc1r6.apps.googleusercontent.com.json")));
-
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                httpTransport, jsonFactory, clientSecrets, Set.of(GMAIL_SEND))
-                .setDataStoreFactory(new FileDataStoreFactory(Paths.get("tokens").toFile()))
-                .setAccessType("offline")
-                .build();
-
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-    }
-
-    public static Gmail getGmailService() throws GeneralSecurityException, IOException {
-        final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
-        Credential credential = getCredentials(httpTransport, jsonFactory);
-        return new Gmail.Builder(httpTransport, jsonFactory, credential)
-                .setApplicationName("Gmail API Java Quickstart")
-                .build();
-    }
-
-    public static MimeMessage createMail(String to, String from, String subject, String bodyText) throws MessagingException {
-        Properties props = new Properties();
-        Session session = Session.getDefaultInstance(props, null);
-
-        MimeMessage email = new MimeMessage(session);
-        email.setFrom(new InternetAddress(from));
-        email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
-        email.setSubject(subject);
-        email.setText(bodyText);
-
-        return email;
-    }
-
-    public static void sendMail(Gmail service, String userId, MimeMessage email) throws MessagingException, IOException {
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        email.writeTo(buffer);
-        byte[] rawEmailBytes = buffer.toByteArray();
-        String encodedEmail = Base64.encodeBase64URLSafeString(rawEmailBytes);
-        
-        Message message = new Message();
-        message.setRaw(encodedEmail);
-        
-        service.users().messages().send(userId, message).execute();
-    }
-
-    public static void main(String[] args) throws Exception {
-        Gmail service = getGmailService();
-        MimeMessage email = createMail("keebcove@gmail.com", "keebcove@gmail.com", "Test Subject", "This is a test email.");
-        sendMail(service, "me", email);
-    }
+	private static final String EMAIL_FROM = "keebcove@gmail.com";
+	private static final String EMAIL_TO = "keebcove@gmail.com";
+	private static final String APP_PASSWORD = "aict dxdp vhkj wwcw";
+	
+	public static void main(String[] args) throws Exception {
+		Message message = new MimeMessage(getEmailSession());
+		message.setFrom(new InternetAddress(EMAIL_FROM));
+		message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(EMAIL_TO));
+		message.setSubject("Email subject");
+		message.setText("This is my email sent from Gmail using Java");
+		Transport.send(message);
+	}
+	
+	private static Session getEmailSession() {
+		return Session.getInstance(getGmailProperties(), new Authenticator() {
+		    protected PasswordAuthentication getPasswordAuthentication() {
+		        return new PasswordAuthentication(EMAIL_FROM, APP_PASSWORD);
+		    }
+		});
+	}
+	
+	private static Properties getGmailProperties() {
+		Properties prop = new Properties();
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.starttls.enable", "true");
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", "587");
+		prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+		return prop;
+	}
 }
